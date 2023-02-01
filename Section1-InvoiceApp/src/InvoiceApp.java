ac8e002b6ed3aee6514a17d29e954f2269453d3a
Tyler Gleason <tygleas@gmail.com>
2023-01-31 22:34:29 -0600

    Now its actually working for real :)

--- Section1-InvoiceApp/src/InvoiceApp.java
+++ Section1-InvoiceApp/src/InvoiceApp.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 /*********************************************************************************************
-Description: 		The invoice app calculates the discount amount
+Description: 		This invoice app calculates the discount amount.
 Programmer’s name: 	Tyler Gleason
 Date Written: 		2023.01.27
 GitHub: 			https://github.com/tygleas1/MyJavaProjects/tree/main/Section1-InvoiceApp
@@ -34,15 +34,25 @@ public class InvoiceApp {
         System.out.println("==========================================");
         System.out.println();  // print a blank line
 
+
         while (!choice.equalsIgnoreCase("n")) {
             // get the number of line items from the user
             System.out.print("Enter the number of line items:   ");
             numLineItems = sc.nextInt();
+            System.out.println();
+            System.out.println("==========================================");
+            System.out.println();
+
+
             
             for (int i = 1; i <= numLineItems; i++) {
             	System.out.print("Enter #" + i + " line item amount: ");
             	subtotal += sc.nextDouble();
             }
+        
+        System.out.println();    
+        System.out.println("==========================================");
+        System.out.println();
 
             // calculate the discount amount and total
             if (subtotal >= 500) {
@@ -64,34 +74,47 @@ public class InvoiceApp {
 
             // display the discount amount and total
             // better is to use printf
+            /*
             message = "Discount percent: " + discountPercent + "\n"
                            + "Discount amount:  " + discountAmount + "\n"
                            + "Invoice total:    " + invoiceDiscountTotal + "\n";            
             System.out.println(message);
+            */
             
-          /*printf
-            printf
-            printf*/
+            System.out.println();
+            System.out.printf("%20s: %,10.2f\n", "Subtotal", subtotal);
+            System.out.printf("%20s: %,10.2f\n", "Discount percent", discountPercent);
+            System.out.printf("%20s: %,10.2f\n", "Discount amount", discountAmount);
+            System.out.printf("%20s: %,10.2f\n", "Invoice total", invoiceDiscountTotal);
 
             sc.nextLine(); // empty the input buffer getting rid of the leftover return character
             
             // see if the user wants to continue
+            System.out.println();
+            System.out.println("==========================================");
+            System.out.println();
             System.out.print("Continue? (y/n): ");
             choice = sc.nextLine();
+            System.out.println();
+            System.out.println("==========================================");
             System.out.println();
         }
         
         // calculate and display invoice count, average invoice, and average discount
         // better way is to use printf
+        /*
         message = "Number of invoices: " + invoiceCount + "\n"
                        + "Average invoice:    " + invoiceTotal / invoiceCount + "\n"
                        + "Average discount:   " + discountTotal / invoiceCount + "\n";
         System.out.println(message);
+        */
         
         
-      /*printf
-        printf
-        printf */
+        System.out.printf("%20s: %,10.2f\n", "Number of invoices", invoiceCount);
+        System.out.printf("%20s: %,10.2f\n", "Average invoice", invoiceTotal / invoiceCount);
+        System.out.printf("%20s: %,10.2f\n", "Average discount", discountTotal / invoiceCount);
+        
+        //System.out.print();
         
         sc.close();
     } // end of main
