4c24153e0efff592383ae3e36c669f53306fe664
Tyler Gleason <tygleas@gmail.com>
2023-04-06 15:22:02 -0500

    updated the code

--- Zoo/src/Animal.java
+++ Zoo/src/Animal.java
@@ -5,23 +5,23 @@ import java.util.Locale;
 public class Animal {
 
 	private static int counter = 0;
-	
+
 	private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("MM-dd-yyyy", Locale.US);
 
 	private int id;
-	
+
 	private final String type;
-	
+
 	private Gender gender;
-	
+
 	/**
 	 * https://www.baeldung.com/java-creating-localdate-with-values
 	 * https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
 	 */	
 	private LocalDate birthdate;
-	
+
 	private float weight;
-	
+
 	public Animal() {
 		this.id = ++Animal.counter;
 		this.type = "Animal";
@@ -29,7 +29,7 @@ public class Animal {
 		this.birthdate = null;
 		this.weight = 0;
 	}
-	
+
 	public <T> Animal(T birthdate, float weight) throws Exception {
 		this();
 		this.setBirthdate(birthdate);
@@ -39,7 +39,7 @@ public class Animal {
 	public int getId() {
 		return this.id;
 	}
-	
+
 	public LocalDate getBirthdate() {
 		return this.birthdate;
 	}
@@ -50,50 +50,50 @@ public class Animal {
 		else
 			return Animal.FORMAT.format(this.birthdate);
 	}
-	
+
 	public Gender getGender() {
 		return this.gender;
 	}
-	
+
 	public String getType() {
 		return this.getType();
 	}
-	
+
 	public float getWeight() {
 		return this.weight;
 	}
-	
+
 	public <T> void setBirthdate(T birthdate) throws Exception {
 
 
 		if (birthdate instanceof String) {
-			
+
 			this.birthdate = LocalDate.parse((String) birthdate, Animal.FORMAT);;
 			/* or for readability:
 				String s = (String) birthdate;
 		    	this.birthdate = LocalDate.parse(s, Animal.FORMAT);; 
-		    */
-		    
+			 */
+
 		} else if (birthdate instanceof LocalDate){
-			
+
 			LocalDate date = (LocalDate) birthdate;
 			this.birthdate = date;
-			
+
 		} else {
-			
+
 			throw new Exception("Invalid date MM-dd-yyyy: " + birthdate);
-			
+
 		}
-			
+
 	}
-	
+
 	public <T> void setGender(T gender) throws Exception {
-		
+
 		if (gender instanceof String) {
 			String s = (String) gender;
-			
+
 			s = s.toLowerCase();
-			
+
 			switch (s) {
 			case "m":
 			case "male":
@@ -105,15 +105,17 @@ public class Animal {
 			default:
 				throw new Exception("Invalid gender: " + s);
 			}
+			
 		} else if (gender instanceof Gender) {
 			this.gender = (Gender) gender;
+			
 		} else {
 			throw new Exception("Invalid gender: " + gender);
 		}
 	}
-	
+
 	public void setWeight(float weight) throws Exception {
-		
+
 		if (weight > 0)
 			this.weight = weight;
 		else
@@ -124,5 +126,5 @@ public class Animal {
 	public String toString() {
 		return this.id + " " + this.type;
 	}
-	
+
 }
--- Zoo/src/Zoo_Keeper_App.java
+++ Zoo/src/Zoo_Keeper_App.java
@@ -3,7 +3,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class Zoo_Keeper_App {
-	
+
 	private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("MM-dd-yyyy", Locale.US);
 
 	public Zoo_Keeper_App() {
@@ -11,33 +11,33 @@ public class Zoo_Keeper_App {
 	}
 
 	public static void main(String[] args) throws Exception {
-	    
+
 		try {
 			Animal a1 = new Animal();
 			Animal a2 = new Animal("12-31-2022", (float) 10.5);
-			
+
 			LocalDate birthdate = LocalDate.parse("11-30-2022", FORMAT);
 			Animal a3 = new Animal(birthdate, (float) 5.5);
-			
+
 			System.out.println(a1);
 			System.out.println(a2);
 			System.out.println(a3);
-			
+
 			System.out.println();
-			
+
 			System.out.println(a1.getBirthdateStr() + " " + a1.getWeight());
 			System.out.println(a2.getBirthdateStr() + " " + a2.getWeight());
 			System.out.println(a3.getBirthdateStr() + " " + a3.getWeight());
-			
+
 			a2.setGender("Male");
 			a3.setGender(Gender.FEMALE);
-			
+
 			//System.out.println(a2)
-			
+
 		} catch (Exception e) {
 			System.out.println("The following error has occurred: ");
 			System.out.println(e.getMessage());
-			
+
 		}
 	}
 
